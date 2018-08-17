0e32c024746f81d1bf726230b933e91387785a10
UpcraftLP <j-schirra@gmx.de>
2018-08-17 22:42:27 +0200

    update mixin priority

--- src/main/java/com/github/upcraftlp/ghostbuster/mixin/MixinPhantomSpawner.java
+++ src/main/java/com/github/upcraftlp/ghostbuster/mixin/MixinPhantomSpawner.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PhantomSpawner.class)
+@Mixin(value = PhantomSpawner.class, priority = 500)
 public class MixinPhantomSpawner {
 
     @Inject(method = "spawnMobs", at = @At("HEAD"), cancellable = true)
