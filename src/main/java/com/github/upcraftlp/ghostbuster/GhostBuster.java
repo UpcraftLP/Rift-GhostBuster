b9b517db57d2b8d9563b3e21cf2bda3833d49bd0
UpcraftLP <j-schirra@gmx.de>
2018-08-22 21:24:11 +0200

    fix mixin not working properly

--- src/main/java/com/github/upcraftlp/ghostbuster/GhostBuster.java
+++ src/main/java/com/github/upcraftlp/ghostbuster/GhostBuster.java
@@ -2,15 +2,19 @@ package com.github.upcraftlp.ghostbuster;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.dimdev.rift.listener.MinecraftStartListener;
+import org.dimdev.riftloader.listener.InitializationListener;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
 
 @SuppressWarnings("unused")
-public class GhostBuster implements MinecraftStartListener {
+public class GhostBuster implements InitializationListener {
 
     private static final Logger log = LogManager.getLogger("GhostBuster");
 
     @Override
-    public void onMinecraftStart() {
+    public void onInitialization() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.ghostbuster.json");
         log.info("If there's something strange in you neighborhood,");
         log.info("who you gonna call? Ghostbusters!");
     }
